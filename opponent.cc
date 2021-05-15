19fdc398046e42176f74544830c334cf46ae5801
Samir Shivji <78053503+samir0shivji@users.noreply.github.com>
2021-05-14 20:40:32 -0700

    Update opponent.cc

---
+++ opponent.cc
@@ -0,0 +1,30 @@
+#include "opponent.h"
+
+void Opponent::Move(const graphics::Image& image) {
+  SetY(GetY() + 3);
+  SetX(GetX() + 5);
+  if (IsOutOfBounds(image)) {
+    SetIsActive(false);
+  }
+}
+
+void OpponentProjectile::Move(const graphics::Image& image) {
+  SetY(GetY() + 3);
+  if (IsOutOfBounds(image)) {
+    SetIsActive(false);
+  }
+}
+
+std::unique_ptr<OpponentProjectile> Opponent::LaunchProjectile(){
+  std::unique_ptr<OpponentProjectile> projectile;
+  int counter = 0;
+  if(counter < 5){
+    counter ++;
+    projectile = nullptr;
+    return projectile;
+  }else{
+    projectile = std::make_unique<OpponentProjectile>(object->GetX(), object->GetY());
+    counter = 0;
+    return std::move(projectile);
+  }
+}
